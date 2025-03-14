7c4dd61df9602494755cc81836124c9daa55e7ea
mohamedashraf001 <mohamedashraf11ma11@gmail.com>
2025-03-14 18:39:49 +0200

    First commit

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import express  from "express";
+
+import os from "os";
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send(`Hello from ${os.hostname()}!`);
+    });
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+    });    
